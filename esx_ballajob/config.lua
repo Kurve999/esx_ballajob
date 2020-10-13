af0f11f782471e4b4c40c68bb9ce166fce4f4b04
Kurve999 <72327551+Kurve999@users.noreply.github.com>
2020-10-13 16:48:16 -0400

    Add files via upload

---
+++ esx_ballajob/config.lua
@@ -0,0 +1,20 @@
+Config                            = {}
+
+Config.DrawDistance               = 10.0 -- How close do you need to be for the markers to be drawn (in GTA units).
+Config.MarkerType                 = {Cloakrooms = 20, Armories = 21, BossActions = 22, Vehicles = 36, Helicopters = 34}
+Config.MarkerSize                 = {x = 1.5, y = 1.5, z = 0.5}
+Config.MarkerColor                = {r = 50, g = 50, b = 204}
+
+Config.EnablePlayerManagement     = false -- Enable if you want society managing.
+Config.EnableArmoryManagement     = false
+Config.EnableESXIdentity          = false -- Enable if you're using esx_identity.
+Config.EnableLicenses             = false -- Enable if you're using esx_license.
+
+
+Config.EnableJobBlip              = false -- Enable blips for cops on duty, requires esx_society.
+Config.EnableCustomPeds           = false -- Enable custom peds in cloak room? See Config.CustomPeds below to customize peds.
+
+Config.EnableESXService           = false -- Enable esx service?
+Config.MaxInService               = -1 -- How much people can be in service at once?
+
+Config.Locale                     = 'en'
---
+++ esx_ballajob/localization/en_esx_ballajob.sql
@@ -0,0 +1,27 @@
+USE `es_extended`;
+
+INSERT INTO `addon_account` (name, label, shared) VALUES
+	('society_balla', 'Balla', 1)
+;
+
+INSERT INTO `datastore` (name, label, shared) VALUES
+	('society_balla', 'Balla', 1)
+;
+
+INSERT INTO `addon_inventory` (name, label, shared) VALUES
+	('society_balla', 'Balla', 1)
+;
+
+INSERT INTO `jobs` (name, label) VALUES
+	('balla', 'Balla')
+;
+
+INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
+	('balla',0,'youngin','Youngin',150,'{}','{}'),
+	('balla',1,'soldier','Soldier',300,'{}','{}'),
+	('balla',2,'enforcer','Enforcer',500,'{}','{}'),
+	('balla',3,'shot caller','Shot Caller',750,'{}','{}'),
+	('balla',4,'head honcho','Head Honcho',900,'{}','{}'),
+	('balla',5,'boss','Boss',1000,'{}','{}')
+;
+
